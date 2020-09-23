34b6e998d0b4af85eff2e6a309abf737e0570d20
dsone <dsone@users.noreply.github.com>
2020-09-23 19:21:30 +0200

    Ported primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+
+		colors: {
+			primary: {
+				50: '#F5F6F7',
+				100: '#EBEDEF',
+				200: '#CCD2D7',
+				300: '#AEB6BF',
+				400: '#71808E',
+				500: '#34495E',
+				600: '#2F4255',
+				700: '#1F2C38',
+				800: '#17212A',
+				900: '#10161C',
+			},
+		}
+
+	},
   },
   variants: {},
   plugins: [
