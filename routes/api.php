7d04ecce36b3d3f952ac4ea4dc53484139c84498
dsone <dsone@users.noreply.github.com>
2020-10-24 22:35:55 +0200

    [ch260] Moved Type web routes to api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OptionsController;
 use App\Http\Controllers\RSSController;
+use App\Http\Controllers\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,12 @@ use App\Http\Controllers\RSSController;
 */
 
 Route::group([ 'prefix' => '/v1', 'middleware' => [ 'auth' ] ], function () {
-	Route::put('/{user}/options', [ OptionsController::class, 'update' ])->name('user-options-update')->middleware('auth');
+	Route::put('/{user}/options', [ OptionsController::class, 'update' ])->name('user-options-update');
 
 	Route::get('/options/rss', [ RSSController::class, 'changeToken' ])->name('api-refresh-rss');
+
+	Route::post('/type/order', [ TypeController::class, 'order' ])->name('api-type-order');
+	Route::post('/type', [ TypeController::class, 'store' ])->name('api-type-store');
+	Route::put('/type', [ TypeController::class, 'update' ])->name('api-type-update');
+	Route::delete('/type', [ TypeController::class, 'destroy' ])->name('api-type-destroy');
 });
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\OptionsController;
-use App\Http\Controllers\TypeController;
 
 
 /*
@@ -29,8 +28,6 @@ Route::group([ 'prefix' => '/profile' ], function() {
 	Route::get('/{user}', [ ProfileController::class, 'index' ])->name('user-profile');
 
 	Route::get('/{user}/options', [ OptionsController::class, 'index' ])->name('user-options')->middleware('auth');
-	Route::post('/{user}/type', [ TypeController::class, 'store' ])->name('user-type-store')->middleware('auth');
-	Route::delete('/{user}/type/{type}', [ TypeController::class, 'destroy' ])->name('user-type-destroy')->middleware('auth');
 });
 
 Route::group([ 'prefix' => '/page' ], function() {
