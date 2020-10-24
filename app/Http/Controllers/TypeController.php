aa2091a0aa5f7bfd82684cbcf447a57eec9217b0
dsone <dsone@users.noreply.github.com>
2020-10-24 22:40:28 +0200

    [ch260] Implemented missing functions

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -37,10 +37,12 @@ class TypeController extends Controller
 		$user = Auth::user();
 		$type = Type::whereName($request->name)->where('user_id', $user->id)->first();
 		if ($type) {
-			return redirect()->route('user-options', [ 'user' => $user->name ]);
+			return response()->json([
+				'error' => true, 'message' => 'Type exists already!'
+			]);
 		}
 
-		Type::create([
+		$type = Type::create([
 			'user_id'	=> $user->id,
 			'name'		=> $request->name,
 			'ident_1'	=> $request->ident_1,
@@ -49,43 +51,112 @@ class TypeController extends Controller
 			'sort'		=> Type::count()+1,
 		]);
 
-		return redirect()->route('user-options', [ 'user' => $user->name ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Type $type)
-    {
-        //
+		return response()->json([
+			'error' => false, 'message' => 'Type added!', 'data' => $type
+		]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Type  $type
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Type $type)
-    {
-        //
+    public function update(Request $request)
+	{
+        $this->validate($request, [
+			'id'		=> 'required|integer',
+			'name'		=> 'required|string',
+			'ident_1'	=> 'required|string',
+			'ident_2'	=> 'required|string',
+            'display'	=> 'required|integer',
+        ]);
+
+		$user = Auth::user();
+		$type = Type::whereId($request->id)->where('user_id', $user->id)->first();
+		if (!$type) {
+			return response()->json([
+				'error' => true, 'message' => 'Type does not exist!'
+			]);
+		}
+
+		try {
+			$type = $type->update([
+				'name'		=> $request->name,
+				'ident_1'	=> $request->ident_1,
+				'ident_2'	=> $request->ident_2,
+				'display'	=> $request->display,
+			]);
+		} catch (\Exception $e) {
+			return response()->json([
+				'error' => true, 'message' => 'Update failed. DB is busy or unique constraints failed.'
+			]);
+		}
+
+		return response()->json([
+			'error' => false, 'message' => 'Type updated!'
+		]);
     }
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  string				$userName
-     * @param  \App\Models\Type		$type
+	 * 
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function destroy(string $userName, Type $type)
+    public function destroy(Request $request)
     {
-        $b = $type->delete();
+		$this->validate($request, [
+			'id'		=> 'required|integer',
+        ]);
+		$type = Auth::user()->types()->find($request->id);
 
-		return redirect()->route('user-options', [ 'user' => Auth::user()->name ]);
+		if ($type) {
+			$type->delete();
+
+			return response()->json([
+				'error' => false,
+				'message' => 'Type removed',
+				'data' => []
+			]);
+		}
+
+		return response()->json([
+			'error' => true, 'message' => 'Type not found!'
+		]);
+    }
+
+	/**
+     * Reorders the user's types.
+	 * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function order(Request $request)
+    {
+		$this->validate($request, [
+			'order'	=> 'required|array'
+        ]);
+
+		$order = $request->order;
+		if (Auth::user()->types->count() != count($order)) {
+			return response()->json([
+				'error' => true, 'message' => 'Missing Types to update order.'
+			]);
+		}
+
+		$mapOrder = [];
+		foreach ($order as $index => $id) {
+			$mapOrder[$id] = $index;
+		}
+
+		$types = Auth::user()->types;
+		foreach ($types as $type) {
+			$type->update([ 'sort' => $mapOrder[$type->id] ]);
+		}
+		
+		return response()->json([
+			'error' => false, 'message' => 'Types reorderd!'
+		]);
     }
 }
