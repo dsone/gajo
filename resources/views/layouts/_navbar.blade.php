37ce982465edbf95721b075aed0387a44bc8c9db
dsone <dsone@users.noreply.github.com>
2020-11-01 22:44:58 +0100

    Hiding Add button, when no Types available

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -27,7 +27,7 @@
 				</div>
 				<nav class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row js-menu-items">
 					@auth
-						@if (Request::routeIs('user-profile') && $ownProfile)
+						@if (Request::routeIs('user-profile') && $ownProfile && count($types) > 0)
 						<button class="hidden px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 focus:outline-none focus:shadow-outline md:inline-block js-navbar-add-entry" href="#">
 							Add
 						</button>
