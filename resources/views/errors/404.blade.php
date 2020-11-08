2e4a403b4276fd9cc17958949cd9e0dfba7af304
dsone <dsone@users.noreply.github.com>
2020-11-08 13:42:01 +0100

    [ch303] Added 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+@section('meta_index', 'noindex,nofollow')
+@section('title', '404 - Not Found')
+
+@section('content')
+<div class="w-full xl:w-2/3 xl:mx-auto">
+	<div class="w-full px-4 pt-10 md:px-0 md:mx-auto md:w-1/2">
+		<section>
+			<div class="p-10 bg-white rounded shadow-md">
+				<h1 class="text-3xl font-bold">
+					404 
+					<small class="text-md">
+						
+					</small>
+				</h1>
+				<h2 class="text-xl">
+					Not Found
+				</h2>
+				<div class="mt-4 text-md">
+					<p>
+						The page you were looking for does not exist.
+					</p>
+					<p class="mt-4 text-lg">Go back <a class="font-semibold underline" href="/">home</a>.</p>
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+@endsection
