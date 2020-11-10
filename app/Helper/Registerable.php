efc20c6f5e15a39001c8673a4bf9d0a49134c605
dsone <dsone@users.noreply.github.com>
2020-11-10 21:09:48 +0100

    Refactored multi user settings

--- app/Helper/Registerable.php
+++ app/Helper/Registerable.php
@@ -5,11 +5,16 @@ use App\Models\User;
 // Checks if users can register or not
 function registerable() {
     // multiUser env, check if limit is reached - if there is a limit set
-    if (config('gajo.settings.multiUser')) {
-        // limit reached
-        if (config('gajo.settings.userLimit') > 0 && config('gajo.settings.userLimit') <= User::count()) {
-            return false;
+    if (config('gajo.settings.multiUser') !== false) {
+        // no limit on users
+		if (config('gajo.settings.multiUser') === true) {
+			return true;
+		// limit not yet reached
+		} else if (User::count() < config('gajo.settings.multiUser')) {
+            return true;
         }
+
+		return false;
     // no multiUser env set and more than one user, abort
     } else if (User::count() > 0) {
         return false;
--- config/gajo.php
+++ config/gajo.php
@@ -3,12 +3,12 @@
 return [
     'contact_mail' => env('APP_CONTACT_MAIL', ''),
     'settings' => [
-        // If you use this app with more than one user, set this to true
-        // Otherwise only one user can register
-        'multiUser' => false,
-        // If multiUser is set to true, you can limit the amount of users here,
-        // 0 means there is no limit
-        'userLimit' => 0,
+        /*
+		 * If you use this app with more than one user, set this to true for unlimited users.
+		 * Set it to a number to limit a certain amount of users.
+		 * If set to false, only one user can register.
+		 */
+        'multiUser' => env('GAJO_MULTIUSER', false),
         'list' => [
             // constants, do not change
             'visibility' => [
