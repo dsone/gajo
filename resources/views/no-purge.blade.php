a7b4b84b8ccfab6cd1e37b81b3816201ffffb004
dsone <dsone@users.noreply.github.com>
2020-11-14 01:11:38 +0100

    Addition of no-purge template, containing dynamic classes not to purge

---
+++ resources/views/no-purge.blade.php
@@ -0,0 +1,14 @@
+entry-visibility--red
+entry-visibility--orange
+entry-visibility--green
+card-visibility--red
+card-visibility--orange
+card-visibility--green
+icon-red
+icon-orange
+icon-green
+
+release-available
+release-later
+release-48-hours
+release-1-week
